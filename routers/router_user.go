967397f1c2260dbbf404fe4f5c176eb92d56271f
Wang <wanghp5520jlu@outlook.com>
2024-11-25 09:50:12 +0800

    2024_11_25_router_user

--- routers/router_user.go
+++ routers/router_user.go
@@ -1,1 +1,16 @@
 package routers
+
+import (
+	v1 "panel_backend/api/v1"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserManageRouter(router *gin.Engine) {
+	//用户管理相关路由
+	// router.POST("/updateUser", v1.UpdateUser)
+	router.POST("/deleteAccount", v1.DeleteUser)
+
+	router.GET("/getUserName", v1.GetUserName)
+	router.POST("/modifyPassword", v1.ModifyPassword)
+}
