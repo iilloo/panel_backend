ce68c8989c0d1cd3494fc11fdfb5789da720faac
Wang <wanghp5520jlu@outlook.com>
2024-11-28 23:17:53 +0800

    2024_11_28_timing_task

--- repository/timing_task.go
+++ repository/timing_task.go
@@ -1,13 +1,25 @@
 package repository
-func AddTask() {
+
+import (
+	"panel_backend/global"
+	"panel_backend/models"
+)
+func AddTask(timingTask models.TimingTask) error {
 	//添加定时任务
+	err := global.DB.Create(&timingTask).Error
+	return err
 }
-func DeleteTask() {
+func DeleteTask(taskName string) error {
 	//删除定时任务
+	err := global.DB.Where("task_name = ?", taskName).Delete(&models.TimingTask{}).Error
+	return err
 }
 func UpdateTask() {
 	//更新定时任务
 }
-func GetTaskList() {
+func GetTaskList() ([]models.TimingTask, error){
 	//获取定时任务列表
+	var timingTasks []models.TimingTask
+	err := global.DB.Find(&timingTasks).Error
+	return timingTasks, err
 }
